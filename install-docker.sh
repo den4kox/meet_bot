2f9d4d68bc82fd5e6422ce13318bdfe274aa6481
Kokhnovich Denis <dkokhnovich@marathonbet.ru>
2018-04-25 12:53:06 +0300

    upd docker-compose

--- install-docker.sh
+++ install-docker.sh
@@ -6,3 +6,6 @@ sudo apt-add-repository 'deb https://apt.dockerproject.org/repo ubuntu-xenial ma
 sudo apt-get update
 sudo apt-get install -y docker-engine
 sudo usermod -aG docker $(whoami)
+
+sudo curl -L https://github.com/docker/compose/releases/download/1.18.0/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
