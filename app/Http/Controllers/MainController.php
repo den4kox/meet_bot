c3f66d373a3ba05b852b44b53973d13df5f21eaa
Kokhnovich Denis <dkokhnovich@marathonbet.ru>
2018-04-27 18:05:30 +0300

    Revert: upd

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -3,6 +3,9 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use GuzzleHttp\Exception\GuzzleException;
+use GuzzleHttp\Client;
+
 // https://api.telegram.org/bot528975393:AAGixyvKXmLFEDBcEBjeqXL3-WxPYq41RvQ/setWebhook
 class MainController extends Controller
 {
@@ -10,33 +13,28 @@ class MainController extends Controller
     {
         $this->token = '528975393:AAGixyvKXmLFEDBcEBjeqXL3-WxPYq41RvQ';
         $this->url = 'https://api.telegram.org/bot528975393:AAGixyvKXmLFEDBcEBjeqXL3-WxPYq41RvQ/';
+
+        $apiKey = '528975393:AAGixyvKXmLFEDBcEBjeqXL3-WxPYq41RvQ'; // Put your bot's API key here
+        $apiURL = 'https://api.telegram.org/bot' . $apiKey . '/';
+
+        $this->client = new Client( array( 'base_uri' => $apiURL ) );
     }
 
     public function get(Request $request) {
         // https://api.telegram.org/bot528975393:AAGixyvKXmLFEDBcEBjeqXL3-WxPYq41RvQ/sendMessage
+        $resp = $this->client->post('sendMessage', 
+            array( 'query' => array( 'chat_id' => '-1001395709569', 'text' => "Янка забиянка" ) ) 
+        );
 
-        $client = new GuzzleHttp\Client();
-        $res = $client
-            ->post('https://api.telegram.org/bot528975393:AAGixyvKXmLFEDBcEBjeqXL3-WxPYq41RvQ/sendMessage', 
-                [
-                    "chat_id" => "-1001395709569",
-                    "text" => "Янка забиянка22"
-                ]
-            );
-        return 'qwe';
+        return 'gooo';
     }
 
     public function post(Request $request) {
         // https://api.telegram.org/bot528975393:AAGixyvKXmLFEDBcEBjeqXL3-WxPYq41RvQ/sendMessage
+        $resp = $this->client->post('sendMessage', 
+            array( 'query' => array( 'chat_id' => '-1001395709569', 'text' => "Янка забиянка" ) ) 
+        );
 
-        $client = new GuzzleHttp\Client();
-        $res = $client
-            ->post('https://api.telegram.org/bot528975393:AAGixyvKXmLFEDBcEBjeqXL3-WxPYq41RvQ/sendMessage', 
-                [
-                    "chat_id" => "-1001395709569",
-                    "text" => "Янка забиянка33"
-                ]
-            );
-        return 'qwe'; 
+        return 'gooo';
     }
 }
