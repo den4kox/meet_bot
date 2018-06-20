197e9c7affa41240e6ce65e7fdcea4e32fcf97a7
Kokhnovich Denis <dkokhnovich@marathonbet.ru>
2018-06-20 11:07:55 +0300

    upd

--- database/migrations/2018_04_25_072908_create_users_table.php
+++ database/migrations/2018_04_25_072908_create_users_table.php
@@ -16,9 +16,9 @@ class CreateUsersTable extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->integer('id')->unsigned();
             $table->primary('id');
-            $table->string('first_name');
-            $table->string('last_name');
-            $table->string('username');
+            $table->string('first_name')->nullable();
+            $table->string('last_name')->nullable();
+            $table->string('username')->nullable();
             $table->timestamps();
         });
     }
